306cbebe8c263afc21a78064250dfda80e7477a2
Davidcparrar <davidc.parrar@gmail.com>
2023-12-22 17:48:54 +0100

    feat: add read file from terminal

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,33 @@
-fn main() {
-    println!("Hello, world!");
-}
+use std::env;
 use std::fs::File;
 use std::io::{BufRead, BufReader};
 
 fn main() {
-    let file = File::open("non_existent_file.txt");
+    let args: Vec<String> = env::args().collect();
+    println!("My path is {}.", args[0]);
+    
+    let filename = if args.len() > 1 {
+        println!("Input file is {}.", args[1]);
+        args[1].clone()
+    }
+    else {
+        println!("No input file specified.");
+        String::from("input.txt")
+    };
+
+    let file = File::open(filename);
     let file = match file {
         Ok(file) => file,
-        Err(error) => {
-            match error.kind() {
-                std::io::ErrorKind::NotFound => {
-                    panic!("File not found: {}", error)
-                }
-                _ => {
-                    panic!("Error opening file: {}", error)
-                }
+        Err(error) => match error.kind() {
+            std::io::ErrorKind::NotFound => {
+                panic!("File not found: {}", error)
             }
-        }
+            _ => {
+                panic!("Error opening file: {}", error)
+            }
+        },
     };
-    
+
     let reader = BufReader::new(file);
     for line in reader.lines() {
         match line {
